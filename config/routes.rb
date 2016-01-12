70e61fa65686edf449a5e2e2aeaf6190a9e5c473
tatsianab <tnborshch@yahoo.com>
2016-01-12 10:19:13 -0500

    Add edit, show, index actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
-
+  resources :articles
+  get 'about', to: 'pages#about'
   root 'pages#home'
   # get 'pages/home', to: 'pages#home'
 
-  get 'about', to: 'pages#about'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
